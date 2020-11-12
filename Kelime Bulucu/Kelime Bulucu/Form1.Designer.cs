d40d28ff64b09a94afd33acd3f7ad57f884abae2
FarukAsci <74362333+FarukAsci@users.noreply.github.com>
2020-11-12 16:49:02 +0300

    Add files via upload

---
+++ Kelime Bulucu/Kelime Bulucu/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Kelime_Bulucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tblKelimeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.dgvKelime = new System.Windows.Forms.DataGridView();
+            this.lblHarf = new System.Windows.Forms.Label();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSearchLetters = new System.Windows.Forms.Button();
+            this.tbxBegin = new System.Windows.Forms.TextBox();
+            this.tbxMinLetter = new System.Windows.Forms.TextBox();
+            this.tbxEnd = new System.Windows.Forms.TextBox();
+            this.lblBegin = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.lblMinLetter = new System.Windows.Forms.Label();
+            this.tbxInclude = new System.Windows.Forms.TextBox();
+            this.lblInclude = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKelimeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKelime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(130, 175);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(105, 12);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 20);
+            this.txtAra.TabIndex = 2;
+            // 
+            // dgvKelime
+            // 
+            this.dgvKelime.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKelime.Location = new System.Drawing.Point(211, 12);
+            this.dgvKelime.Name = "dgvKelime";
+            this.dgvKelime.Size = new System.Drawing.Size(260, 426);
+            this.dgvKelime.TabIndex = 3;
+            // 
+            // lblHarf
+            // 
+            this.lblHarf.AutoSize = true;
+            this.lblHarf.Location = new System.Drawing.Point(12, 15);
+            this.lblHarf.Name = "lblHarf";
+            this.lblHarf.Size = new System.Drawing.Size(87, 13);
+            this.lblHarf.TabIndex = 4;
+            this.lblHarf.Text = "Aranacak Harfler";
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Location = new System.Drawing.Point(105, 142);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbxNumber.TabIndex = 2;
+            this.tbxNumber.Text = "7";
+            this.tbxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxNumber_KeyPress);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(12, 145);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(80, 13);
+            this.lblNumber.TabIndex = 4;
+            this.lblNumber.Text = "Maksimum Harf";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(130, 415);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 5;
+            this.btnDel.Text = "Sil";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnSearchLetters
+            // 
+            this.btnSearchLetters.Location = new System.Drawing.Point(130, 204);
+            this.btnSearchLetters.Name = "btnSearchLetters";
+            this.btnSearchLetters.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchLetters.TabIndex = 6;
+            this.btnSearchLetters.Text = "Ara2";
+            this.btnSearchLetters.UseVisualStyleBackColor = true;
+            this.btnSearchLetters.Click += new System.EventHandler(this.btnSearchLetters_Click);
+            // 
+            // tbxBegin
+            // 
+            this.tbxBegin.Location = new System.Drawing.Point(105, 38);
+            this.tbxBegin.Name = "tbxBegin";
+            this.tbxBegin.Size = new System.Drawing.Size(100, 20);
+            this.tbxBegin.TabIndex = 2;
+            // 
+            // tbxMinLetter
+            // 
+            this.tbxMinLetter.Location = new System.Drawing.Point(105, 116);
+            this.tbxMinLetter.Name = "tbxMinLetter";
+            this.tbxMinLetter.Size = new System.Drawing.Size(100, 20);
+            this.tbxMinLetter.TabIndex = 2;
+            this.tbxMinLetter.Text = "3";
+            this.tbxMinLetter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxNumber_KeyPress);
+            // 
+            // tbxEnd
+            // 
+            this.tbxEnd.Location = new System.Drawing.Point(105, 90);
+            this.tbxEnd.Name = "tbxEnd";
+            this.tbxEnd.Size = new System.Drawing.Size(100, 20);
+            this.tbxEnd.TabIndex = 2;
+            // 
+            // lblBegin
+            // 
+            this.lblBegin.AutoSize = true;
+            this.lblBegin.Location = new System.Drawing.Point(12, 41);
+            this.lblBegin.Name = "lblBegin";
+            this.lblBegin.Size = new System.Drawing.Size(60, 13);
+            this.lblBegin.TabIndex = 4;
+            this.lblBegin.Text = "İle Başlasın";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(12, 93);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(46, 13);
+            this.lblEnd.TabIndex = 4;
+            this.lblEnd.Text = "İle Bitsin";
+            // 
+            // lblMinLetter
+            // 
+            this.lblMinLetter.AutoSize = true;
+            this.lblMinLetter.Location = new System.Drawing.Point(12, 119);
+            this.lblMinLetter.Name = "lblMinLetter";
+            this.lblMinLetter.Size = new System.Drawing.Size(71, 13);
+            this.lblMinLetter.TabIndex = 4;
+            this.lblMinLetter.Text = "Minimum Harf";
+            // 
+            // tbxInclude
+            // 
+            this.tbxInclude.Location = new System.Drawing.Point(105, 64);
+            this.tbxInclude.Name = "tbxInclude";
+            this.tbxInclude.Size = new System.Drawing.Size(100, 20);
+            this.tbxInclude.TabIndex = 2;
+            // 
+            // lblInclude
+            // 
+            this.lblInclude.AutoSize = true;
+            this.lblInclude.Location = new System.Drawing.Point(12, 67);
+            this.lblInclude.Name = "lblInclude";
+            this.lblInclude.Size = new System.Drawing.Size(38, 13);
+            this.lblInclude.TabIndex = 4;
+            this.lblInclude.Text = "İçersin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 444);
+            this.Controls.Add(this.btnSearchLetters);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblMinLetter);
+            this.Controls.Add(this.lblInclude);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblBegin);
+            this.Controls.Add(this.lblHarf);
+            this.Controls.Add(this.dgvKelime);
+            this.Controls.Add(this.tbxNumber);
+            this.Controls.Add(this.tbxEnd);
+            this.Controls.Add(this.tbxMinLetter);
+            this.Controls.Add(this.tbxInclude);
+            this.Controls.Add(this.tbxBegin);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.btnAra);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(499, 483);
+            this.MinimumSize = new System.Drawing.Size(499, 483);
+            this.Name = "Form1";
+            this.Text = "Kelime Bulucu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblKelimeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKelime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource tblKelimeBindingSource;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.DataGridView dgvKelime;
+        private System.Windows.Forms.Label lblHarf;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSearchLetters;
+        private System.Windows.Forms.TextBox tbxBegin;
+        private System.Windows.Forms.TextBox tbxMinLetter;
+        private System.Windows.Forms.TextBox tbxEnd;
+        private System.Windows.Forms.Label lblBegin;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Label lblMinLetter;
+        private System.Windows.Forms.TextBox tbxInclude;
+        private System.Windows.Forms.Label lblInclude;
+    }
+}
+
---
+++ Kelime Bulucu/Kelime Bulucu/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Kelime_Bulucu
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection con;
+        SQLiteDataAdapter da;
+        SQLiteCommand cmd;
+        DataSet ds;
+        string lastQuery;
+        string abc = "abcçdefgğhıijklmnoöprsştuüvyz";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string sqlForLetters(string word, string beginning, string include, string ending, string maxLetter, string minLetter)
+        {
+            string sqlQuery = "Select * From Kelimeler where ";
+
+            foreach (char l in word)
+            {
+                sqlQuery = sqlQuery + "kelime like '%" + l + "%' and ";
+            }
+
+
+            if (maxLetter.Length != 0)
+            {
+                sqlQuery += "length(kelime) <= " + maxLetter;
+                sqlQuery += " and length(kelime) >= " + minLetter;
+            }
+            else
+            {
+                int lenght = sqlQuery.Length;
+                sqlQuery = sqlQuery.Substring(0, lenght - 5);
+            }
+
+            if (beginning.Length > 0)
+            {
+                sqlQuery += " and kelime like '" + beginning + "%'";
+            }
+
+            if (include.Length > 0)
+            {
+                sqlQuery += " and kelime like '%" + include + "%'";
+            }
+
+            if (ending.Length > 0)
+            {
+                sqlQuery += " and kelime like '%" + ending + "'";
+            }
+
+            return sqlQuery;
+        }
+
+        public string sqlForSomeLetters(string word, string beginning, string include, string ending, string maxLetter, string minLetter)
+        {
+            string sqlQuery = "Select * From Kelimeler where ";
+            bool isInWord = false;
+
+            foreach (char a in abc)
+            {
+                foreach (char l in word)
+                {
+                    if (a.Equals(l))
+                    {
+                        isInWord = true;
+                    }
+                }
+                if (isInWord)
+                {
+                    isInWord = false;
+                }
+                else
+                {
+                    sqlQuery = sqlQuery + "kelime not like '%" + a + "%' and ";
+                }
+            }
+            
+            if (maxLetter.Length != 0)
+            {
+                sqlQuery += "length(kelime) <= " + maxLetter;
+                sqlQuery += " and length(kelime) >= " + minLetter;
+            }
+            else
+            {
+                int lenght = sqlQuery.Length;
+                sqlQuery = sqlQuery.Substring(0, lenght - 5);
+            }
+
+            if (beginning.Length > 0)
+            {
+                sqlQuery += " and kelime like '" + beginning + "%'";
+            }
+
+            if (include.Length > 0)
+            {
+                sqlQuery += " and kelime like '%" + include + "%'";
+            }
+
+            if (ending.Length > 0)
+            {
+                sqlQuery += " and kelime like '%" + ending + "'";
+            }
+
+            return sqlQuery;
+        }
+
+        private void UpdateDB()
+        {
+            con = new SQLiteConnection("Data Source=.\\Kelimeler.db;Version=3;");
+            da = new SQLiteDataAdapter(lastQuery, con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Kelimeler");
+            dgvKelime.DataSource = ds.Tables["Kelimeler"];
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SQLiteConnection("Data Source=.\\Kelimeler.db;Version=3;");
+            da = new SQLiteDataAdapter("Select * From Kelimeler", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Kelimeler");
+            dgvKelime.DataSource = ds.Tables["Kelimeler"];
+            con.Close();
+            lastQuery = "Select * From Kelimeler";
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranacak = txtAra.Text.ToString();
+            string maxLetter = tbxNumber.Text.ToString();
+            string minLetter = tbxMinLetter.Text.ToString();
+            string beginning = tbxBegin.Text.ToString();
+            string include = tbxInclude.Text.ToString();
+            string ending = tbxEnd.Text.ToString();
+            string sqlQuery = sqlForLetters(aranacak, beginning, include, ending, maxLetter, minLetter);
+
+            da = new SQLiteDataAdapter(sqlQuery, con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Kelimeler");
+            dgvKelime.DataSource = ds.Tables["Kelimeler"];
+            con.Close();
+
+            lastQuery = sqlQuery;
+        }
+
+        private void tbxNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            int rowindex = dgvKelime.CurrentCell.RowIndex;
+
+            string word = dgvKelime.Rows[rowindex].Cells[1].Value.ToString();
+
+            cmd = new SQLiteCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "delete from Kelimeler where Kelime='" + word + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            UpdateDB();
+        }
+
+        private void btnSearchLetters_Click(object sender, EventArgs e)
+        {
+            string aranacak = txtAra.Text.ToString();
+            string maxLetter = tbxNumber.Text.ToString();
+            string minLetter = tbxMinLetter.Text.ToString();
+            string beginning = tbxBegin.Text.ToString();
+            string include = tbxInclude.Text.ToString();
+            string ending = tbxEnd.Text.ToString();
+            string sqlQuery = sqlForSomeLetters(aranacak, beginning, include, ending, maxLetter, minLetter);
+
+            da = new SQLiteDataAdapter(sqlQuery, con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Kelimeler");
+            dgvKelime.DataSource = ds.Tables["Kelimeler"];
+            con.Close();
+
+            lastQuery = sqlQuery;
+        }
+
+    }
+}
